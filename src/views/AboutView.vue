0eb691a3c68ed1afa032a4056070e823e404d88a
RichardKim <hiddeneventor@gmail.com>
2022-06-28 16:25:17 +0900

    RouterView, RouterLink 를 사용한 라우팅 처리

---
+++ src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+<template>
+  <div>
+    <h2>About View</h2>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+<template>
+  <div>
+    <h2>Home View</h2>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style scoped></style>
